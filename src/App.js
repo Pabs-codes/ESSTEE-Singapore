040a9d9b0d283daf1d2bc2e7f9373e88df632688
Pabasara Navod <144713553+Pabs-codes@users.noreply.github.com>
2024-01-16 11:46:15 +0530

    jhgc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import AboutR from "./routes/AboutR";
 import HomeR from "./routes/HomeR";
-import {Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ServicesR from "./routes/ServicesR";
 import ProductsR from "./routes/ProductsR";
 
@@ -8,18 +8,10 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomeR />} />
-      <Route exact path="/AboutR" element={<AboutR/>} />
-      <Route exact path="/ServicesR" element={<ServicesR/>} />
-      <Route exact path="/ProductsR" element={<ProductsR/>} />
-      <Route exact path="/CorporationR" element={<ProductsR/>} />
-      <Route exact path="/ContactR" element={<ProductsR/>} />
-
+      <Route path="/About" element={<AboutR />} />
+      <Route path="/Services" element={<ServicesR />} />
+      <Route path="/Products" element={<ProductsR />} />
 
     </Routes>
   );
 }
-
-
-
-
-
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,89 +1,92 @@
-import React, {useState} from 'react'
-import "./navbar.css"
+import React, { useState } from "react";
+import "./navbar.css";
 
 // import {MdTravelExplore} from 'react-icons/md'
-import {AiFillCloseCircle} from 'react-icons/ai'
-import {TbGridDots} from 'react-icons/tb'
-import logoImage from '../../Assets/ESSTEEhq.png'
-
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
+import logoImage from "../../Assets/ESSTEEhq.png";
 
 export const Navbar = () => {
+  const [active, setActive] = useState("navBar");
 
-    const [active, setActive] = useState('navBar') ;
+  // function to toggle navBar
+  const showNav = () => {
+    setActive("navBar activeNavbar");
+  };
 
-    // function to toggle navBar
-    const showNav = ()=> {
-        setActive('navBar activeNavbar');
-    }
-
-    // function to remove navBar
-    const removeNavbar = ()=> {
-        setActive('navBar');
-    }
-
+  // function to remove navBar
+  const removeNavbar = () => {
+    setActive("navBar");
+  };
 
   return (
-    <section className='navBarSection' >
-        <header className='header flex'>
-            <div className='logoDiv'>
-                <a href='/' className='logo flex'>
-                <img
-            className="logo"
-            src={logoImage}
-            alt="Estee Singapoore PTE.Ltd. Logo"
-          />
-                </a>
-            </div>
+    <section className="navBarSection">
+      <header className="header flex">
+        <div className="logoDiv">
+          <a href="/" className="logo flex">
+            <img
+              className="logo"
+              src={logoImage}
+              alt="Estee Singapoore PTE.Ltd. Logo"
+            />
+          </a>
+        </div>
 
-            <div className={active}>
-                <ul className='navList flex'>
+        <div className={active}>
+          <ul className="navList flex">
+            <li className="navItem">
+              <a href="/" className="navLink">
+                Home
+              </a>
+            </li>
 
-                    <li className='navItem'>
-                        <a href='/' className='navLink'>Home</a>
-                    </li>
+            <li className="navItem">
+              <a href="/About" className="navLink">
+                About Us
+              </a>
+            </li>
 
-                    <li className='navItem'>
-                        <a href='/AboutR' className='navLink'>About Us</a>
-                    </li>
+            <li className="navItem">
+              <a href="/Services" className="navLink">
+                Services
+              </a>
+            </li>
 
-                    <li className='navItem'>
-                        <a href='/ServicesR' className='navLink'>Services</a>
-                    </li>
+            <li className="navItem">
+              <a href="/Products" className="navLink">
+                Products
+              </a>
+            </li>
 
-                    <li className='navItem'>
-                        <a href='/ProductsR' className='navLink'>Products</a>
-                    </li>
+            <li className="navItem">
+              <a href="/Corporation" className="navLink">
+                {" "}
+                Corporation Partners
+              </a>
+            </li>
 
-                    <li className='navItem'>
-                        <a href='/CorporationR' className='navLink'> Corporation Partners</a>
-                    </li>
+            <li className="navItem">
+              <a href="/" className="navLink">
+                Contact
+              </a>
+            </li>
 
-                    <li className='navItem'>
-                        <a href='/ContactR' className='navLink'>Contact</a>
-                    </li>
+            <button className="btn">
+              <a href="/">Call Now</a>
+            </button>
+          </ul>
 
+          <div onClick={removeNavbar} className="closeNavbar">
+            <AiFillCloseCircle className="icon" />
+          </div>
+        </div>
 
-                    <button className='btn'>
-                        <a href="/">Call Now</a>
-                    </button>
-
-      
-
-                </ul>
-
-                <div onClick={removeNavbar} className="closeNavbar">
-                    <AiFillCloseCircle className='icon'/>
-                </div>
-
-            </div>
-
-            <div onClick={showNav} className="toggleNavbar">
-                    < TbGridDots className='icon' />
-            </div>
-
-        </header>
+        <div onClick={showNav} className="toggleNavbar">
+          <TbGridDots className="icon" />
+        </div>
+      </header>
     </section>
-  )
-}
+  );
+};
 
 export default Navbar;
