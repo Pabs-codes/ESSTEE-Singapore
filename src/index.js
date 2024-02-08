e8f13eb914281e595ddfc3257b9793e7123a63b3
Pabasara Navod <144713553+Pabs-codes@users.noreply.github.com>
2024-02-08 14:29:56 +0530

    asdfv

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import "./components/Hero/s.css"
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
