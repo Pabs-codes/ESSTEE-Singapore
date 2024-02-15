3a5961d629f7f843f2288c9f1e9ae604c02401c7
Pabasara Navod <144713553+Pabs-codes@users.noreply.github.com>
2024-02-15 12:46:04 +0530

    fgh

--- src/components/Hero/Donutchart.js
+++ src/components/Hero/Donutchart.js
@@ -1,37 +1,35 @@
 import React from "react";
 import Chart from 'react-apexcharts';
+import './Donutchart.css'; // Import the CSS file
 
 function Donutchart() {
-    return (
-        <React.Fragment>
-            <div className='container-fluid mt-3 mb-3 chart-container'>
-                <Chart
-                    type="donut"
-                    width={500} // Adjust width as needed
-                    height={400} // Adjust height as needed
-                    series={[78, 11, 9, 2]}
-
-                    options={{
-                        labels: ['Asia', 'Africa', 'Europe', 'Americas'],
-                        title:{
-                            text:"ESSTEE : Global Trade"
-                        },
-
-                        plotOptions:{
-                            pie:{
-                                donut:{
-                                    labels:{
-                                        show:true,
-                                    }
-                                   
-                                }
-                            }
-                        }
-                    }}
-                />
-            </div>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <div className='container-fluid mt-3 mb-3 chart-container'>
+        <Chart
+          type="donut"
+          width={500}
+          height={400}
+          series={[78, 11, 9, 2]}
+          options={{
+            labels: ['Asia', 'Africa', 'Europe', 'Americas'],
+            title: {
+              text: "ESSTEE : Global Trade"
+            },
+            plotOptions: {
+              pie: {
+                donut: {
+                  labels: {
+                    show: true,
+                  }
+                }
+              }
+            }
+          }}
+        />
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Donutchart;
